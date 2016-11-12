ffe7ba607ee5d62af057dcff7e4c10627882e9ac
Dan Walker <dwalker109@gmail.com>
2016-11-12 16:36:39 +0000

    Adds CLI name requesting and basic game loop stub

--- src/Battle/Battle.php
+++ src/Battle/Battle.php
@@ -5,11 +5,14 @@ namespace dwalker109\Battle;
 use dwalker109\Combatant\Brute;
 use dwalker109\Combatant\Grappler;
 use dwalker109\Combatant\Swordsman;
+use dwalker109\Combatant\Combatant;
 
 class Battle
 {
     private $player_1;
     private $player_2;
+    private $player_current;
+    private $player_next;
     
     // Register available combatant types
     private $combatant_types = [
@@ -31,5 +34,25 @@ class Battle
         $types = array_rand(array_flip($this->combatant_types), 2);
         $this->player_1 = new $types[0]($name_1);
         $this->player_2 = new $types[1]($name_1);
+    }
+    
+    /**
+     * Carry out a single simulation turn.
+     *
+     * @return void
+     */
+    public function calculateTurn()
+    {
+        
+    }
+    
+    /**
+     * Return the next player and update internal reference for next time.
+     *
+     * @return Combatant
+     */
+    private function nextPlayer()
+    {
+        
     }
 }
--- src/Battle/CliBattleCommand.php
+++ src/Battle/CliBattleCommand.php
@@ -2,6 +2,7 @@
 
 namespace dwalker109\Battle;
 
+use dwalker109\Battle\Battle;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,14 +10,67 @@ use Symfony\Component\Console\Question\Question;
 
 class CliBattleCommand extends Command
 {
+    /**
+     * Set up this Symfony command.
+     *
+     * @return void
+     */
     protected function configure()
     {
         $this->setName("battle:run");
         $this->setDescription("Runs a CLI based battle.");
     }
 
+    /**
+     * Run the command.
+     *
+     * @param InputInterface $input, OutputInterface $output
+     *
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $battle = new Battle('arg', 'blarg');
+        // Each combatant's name will be requested and stored here
+        $names = [
+            'first' => null,
+            'second' => null,
+        ];
+        
+        // Get each name from CLI and validate the length
+        foreach ($names as $label => $name) {
+            $helper = $this->getHelper('question');
+            $question = new Question("Please enter the name of the {$label} combatant: ");
+            $question->setValidator(function ($answer) {
+                if (strlen($answer) <= 32) {
+                    throw new \RuntimeException(
+                        'The combatant\'s name must be 32 characters or less.'
+                    );
+                }
+                
+                return $answer;
+            });
+            
+            $names[$label] = $helper->ask($input, $output, $question);
+        }
+        
+        // Create the battle and run the game loop
+        $battle = new Battle($names['first'], $names['second']);
+        
+        while ($battle->is_active) {
+            $battle->calculateTurn();
+            $this->render($battle);
+        }
+    }
+    
+    /**
+     * Output the current 'frame' of the Battle to the CLI.
+     *
+     * @param Battle $battle
+     *
+     * @return void
+     */
+    private function render(Battle $battle)
+    {
+        var_dump($battle);
     }
 }
