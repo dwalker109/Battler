fd7d972c1be30f3e4517a6088dfb78c5e669dde5
Dan Walker <dwalker109@gmail.com>
2016-11-13 14:32:09 +0000

    Adds simple Swordsman unit tests

---
+++ tests/SwordsmanTest.php
@@ -0,0 +1,45 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+use dwalker109\Battle\Battle;
+use dwalker109\Battler\Battler;
+use dwalker109\Battler\Swordsman;
+use dwalker109\Skills\SkillContract;
+use SebastianBergmann\PeekAndPoke\Proxy;
+
+class SwordsmanTest extends TestCase
+{
+    public function testSwordsmanIsCreated()
+    {
+        $battle = $this->getMockBuilder(Battle::class)
+            ->setConstructorArgs(['Battler', 'Battler'])
+            ->getMock();
+
+        $swordsman = new Swordsman('Swordsman', $battle);
+
+        $this->assertInstanceOf(Battler::class, $swordsman);
+        $this->assertInstanceOf(Swordsman::class, $swordsman);
+
+        return $swordsman;
+    }
+
+    /**
+     * @depends testSwordsmanIsCreated
+     */
+    public function testSwordsmanAttributesAreSane(Swordsman $swordsman)
+    {
+        $proxy = new Proxy($swordsman);
+
+        foreach ($proxy->definitions as $key => $values) {
+            $this->assertGreaterThanOrEqual(
+                $proxy->definitions[$key]['min'],
+                $swordsman->attr()->{$key}
+            );
+
+            $this->assertLessThanOrEqual(
+                $proxy->definitions[$key]['max'],
+                $swordsman->attr()->{$key}
+            );
+        }
+    }
+}
