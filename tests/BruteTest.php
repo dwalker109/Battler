f3d4519c18bede9c10bd8a8506f796d27280be60
Dan Walker <dwalker109@gmail.com>
2016-11-13 15:48:16 +0000

    Adds Brute and Grappler unit tests

---
+++ tests/BruteTest.php
@@ -0,0 +1,45 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+use dwalker109\Battle\Battle;
+use dwalker109\Battler\Battler;
+use dwalker109\Battler\Brute;
+use dwalker109\Skills\SkillContract;
+use SebastianBergmann\PeekAndPoke\Proxy;
+
+class BruteTest extends TestCase
+{
+    public function testBruteIsCreated()
+    {
+        $battle = $this->getMockBuilder(Battle::class)
+            ->setConstructorArgs(['Battler', 'Battler'])
+            ->getMock();
+
+        $brute = new Brute('Brute', $battle);
+
+        $this->assertInstanceOf(Battler::class, $brute);
+        $this->assertInstanceOf(Brute::class, $brute);
+
+        return $brute;
+    }
+
+    /**
+     * @depends testBruteIsCreated
+     */
+    public function testBruteAttributesAreSane(Brute $brute)
+    {
+        $proxy = new Proxy($brute);
+
+        foreach ($proxy->definitions as $key => $values) {
+            $this->assertGreaterThanOrEqual(
+                $proxy->definitions[$key]['min'],
+                $brute->attr()->{$key}
+            );
+
+            $this->assertLessThanOrEqual(
+                $proxy->definitions[$key]['max'],
+                $brute->attr()->{$key}
+            );
+        }
+    }
+}
---
+++ tests/GrapplerTest.php
@@ -0,0 +1,45 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+use dwalker109\Battle\Battle;
+use dwalker109\Battler\Battler;
+use dwalker109\Battler\Grappler;
+use dwalker109\Skills\SkillContract;
+use SebastianBergmann\PeekAndPoke\Proxy;
+
+class GrapplerTest extends TestCase
+{
+    public function testGrapplerIsCreated()
+    {
+        $battle = $this->getMockBuilder(Battle::class)
+            ->setConstructorArgs(['Battler', 'Battler'])
+            ->getMock();
+
+        $grappler = new Grappler('Grappler', $battle);
+
+        $this->assertInstanceOf(Battler::class, $grappler);
+        $this->assertInstanceOf(Grappler::class, $grappler);
+
+        return $grappler;
+    }
+
+    /**
+     * @depends testGrapplerIsCreated
+     */
+    public function testGrapplerAttributesAreSane(Grappler $grappler)
+    {
+        $proxy = new Proxy($grappler);
+
+        foreach ($proxy->definitions as $key => $values) {
+            $this->assertGreaterThanOrEqual(
+                $proxy->definitions[$key]['min'],
+                $grappler->attr()->{$key}
+            );
+
+            $this->assertLessThanOrEqual(
+                $proxy->definitions[$key]['max'],
+                $grappler->attr()->{$key}
+            );
+        }
+    }
+}
